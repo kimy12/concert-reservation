7b68aa533ca6b824c8f015af1d835c7d26959f65
kimy12 <shinkim7941@gmail.com>
2025-01-03 03:39:12 +0900

    feat : mock API 테스트 코드

---
+++ src/test/java/kr/hhplus/be/server/api/token/presentation/controller/TokenControllerTest.java
@@ -0,0 +1,45 @@
+package kr.hhplus.be.server.api.token.presentation.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import kr.hhplus.be.server.api.token.presentation.dto.TokenRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TokenController.class)
+class TokenControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+
+    @DisplayName("토큰을 발급받는다")
+    @Test
+    void getToken() throws Exception {
+        // given
+        TokenRequest.Request request = new TokenRequest.Request(1);
+
+        // when // then
+        mockMvc.perform(
+                        post("/token/api/v1")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.token").value("fcdba094-a7b2-4bb3-b646-eb143cb83ee0"))
+                .andExpect(jsonPath("$.data.status").value("Pending"));
+    }
+}
