a0e7d943c8e68c58f06577c9b476caecc9024a47
kimy12 <shinkim7941@gmail.com>
2025-01-03 04:04:47 +0900

    Revert "chore : build 수정"
    This reverts commit c92d7973486981772400e768e6347f6da3fe0867.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.asciidoctor.jvm.convert") version "4.0.4"
 }
 
 fun getGitHash(): String {
@@ -13,15 +12,12 @@ fun getGitHash(): String {
 
 group = "kr.hhplus.be"
 version = getGitHash()
-val asciidoctorExt: Configuration by configurations.creating
 
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(17)
 	}
 }
-
-
 
 repositories {
 	mavenCentral()
@@ -34,59 +30,20 @@ dependencyManagement {
 }
 
 dependencies {
-	// Spring
+    // Spring
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
-	// DB
+    // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
-	// Lombok
-	compileOnly("org.projectlombok:lombok:1.18.28") // Lombok 추가
-	annotationProcessor("org.projectlombok:lombok:1.18.28")
-
-	// Test
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:mysql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	// RestDoc
-	asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
-	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-}
-
-
-
-val snippetsDir by extra { file("build/generated-snippets") }
-
-tasks {
-
-	// 5. test Task snippetsDir 추가
-	test {
-		outputs.dir(snippetsDir)
-		useJUnitPlatform()
-	}
-
-	// 6. asciidoctor Task 추가
-	asciidoctor {
-		inputs.dir(snippetsDir)
-		configurations("asciidoctorExt")
-		sources{
-			include("**/index.adoc")
-		}
-		baseDirFollowsSourceFile()
-		dependsOn(test)
-	}
-
-	// 7. bootJar Settings
-	bootJar {
-		dependsOn(asciidoctor)
-		from ("build/docs/asciidoc") {
-			into("static/docs")
-		}
-	}
 }
 
 tasks.withType<Test> {
